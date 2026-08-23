e313e18598a562d2d123c8a7ac75b47b14315bc8
ShadowRoot <mouhamedmoustaphasabaly711@gmail.com>
2026-08-23 17:11:43 +0000

    added one feature instead of usage message when no argument is given there is single-setting mode when one arg is given, all-settings default mode when no arg is give and also i add 3 more setting we can check PermitEmptyPasswords, X11Forwarding, PubkeyAuthentication

--- Bash_Scripting/HardeningChecker/HardeningChecker.sh
+++ Bash_Scripting/HardeningChecker/HardeningChecker.sh
@@ -7,15 +7,27 @@
 set -u
 
 SettingName=${1:-}
+default_settings=("PermitRootLogin" "PasswordAuthentication" "PermitEmptyPasswords" "X11Forwarding" "PubkeyAuthentication")
 
 # Declare associative arrays
-declare -A PermitRootLogin PasswordAuthentication
+declare -A PermitRootLogin PasswordAuthentication PermitEmptyPasswords X11Forwarding PubkeyAuthentication
 PermitRootLogin["no"]="[PASS] Root login disabled entirely"
 PermitRootLogin["yes"]="[FAIL] Root login allowed with password (high risk)"
 PermitRootLogin["prohibit-password"]="[PASS] Root login restricted to key-based auth (reasonably safe)"
+
 PasswordAuthentication["no"]="[PASS] Disables password login and forces SSH key-based authentication (safer)"
 PasswordAuthentication["yes"]="[FAIL] allows password login, which can be targeted by brute-force attacks (riskier)"
 
+PermitEmptyPasswords["no"]="[PASS] Empty passwords are not allowed"
+PermitEmptyPasswords["yes"]="[FAIL] Empty passwords are allowed — critical risk"
+
+X11Forwarding["no"]="[PASS] X11 forwarding disabled"
+X11Forwarding["yes"]="[WARNING] X11 forwarding enabled — only needed for GUI app forwarding, disable if unused"
+
+PubkeyAuthentication["yes"]="[PASS] Key-based authentication is enabled"
+PubkeyAuthentication["no"]="[FAIL] Key-based authentication is disabled — relies on weaker auth methods"
+
+# This Function tells you the verdict based on the value_to_check
 lookup_verdict(){
 	# Store the arguments passed to the function in these variables
 	local array_name="$1"
@@ -25,7 +37,7 @@ lookup_verdict(){
 	declare -n table="$array_name"
 
 	# -v tests whether this specific array KEY is set at all
-	if declare -p $array_name &>/dev/null 
+	if declare -p "$array_name" &>/dev/null 
 	then
 		if [[ -v table["$value_to_check"] ]]
 		then
@@ -38,43 +50,50 @@ lookup_verdict(){
 	fi
 }
 
-usage(){
-        echo -e "\nusage:\n\t$0 <SettingName> [...]\n" >&2
-}
+# This Function read "/etc/ssh/sshd_config" file, search for all the line that has our key word 
+# and output them 
+# if they are settings it tells you what configuration are on set 
+# if they are just comments it tells you too
+Reporter(){
+	local SettingName="$1"
+	echo "--- Checking $SettingName ---"
 
-matches_found=0
+	matches_found=0
+
+	while read -r line
+	do	
+		matches_found=$((matches_found + 1))
+        	echo ""
+        	echo "$line"
+
+        	# Check if line is commented or not
+	
+        	if [[ "$line" =~ ^[[:space:]]*# ]]
+        	then
+                	echo "This line is Commented"
+                	echo ""
+		
+		elif [[ "$line" =~ ^[[:space:]]*$SettingName ]]
+		then
+                
+			keyword=$(echo "$line" | awk '{ print $2 }')
+			lookup_verdict "$SettingName" "$keyword"
+    			echo ""
+        	fi
+
+	done < <(grep "$SettingName" "/etc/ssh/sshd_config")
+
+	if [[ $matches_found -eq 0 ]]
+	then
+        	echo "$SettingName was not found"
+	fi
+}
 
 if [[ -z $SettingName ]]
 then
-        usage
-        exit 1
+	for s in "${default_settings[@]}"; do
+		Reporter "$s"
+    	done
+else
+	Reporter "$SettingName"
 fi
-
-while read -r line
-do	
-	matches_found=$((matches_found + 1))
-        echo ""
-        echo "$line"
-
-        # Check if line is commented or not
-	
-        if [[ "$line" =~ ^[[:space:]]*# ]]
-        then
-                echo "This line is Commented"
-                echo ""
-		
-	elif [[ "$line" =~ ^[[:space:]]*$SettingName ]]
-	then
-                
-		keyword=$(echo "$line" | awk '{ print $2 }')
-		lookup_verdict "$SettingName" "$keyword"
-    		echo ""
-        fi
-
-done < <(grep "$SettingName" "/etc/ssh/sshd_config")
-
-if [[ $matches_found -eq 0 ]]
-then
-        echo "$SettingName was not found"
-fi
-
