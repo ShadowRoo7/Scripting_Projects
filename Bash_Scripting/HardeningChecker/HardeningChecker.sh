4cc254f4e0061b3d94c9dadcfbbb2f214faa7f77
ShadowRoot <mouhamedmoustaphasabaly711@gmail.com>
2026-08-23 13:59:57 +0000

    PasswordAuthentication setting check addded

--- Bash_Scripting/HardeningChecker/HardeningChecker.sh
+++ Bash_Scripting/HardeningChecker/HardeningChecker.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
+
 # It's a script that goes around the system, checks a bunch of these settings, and tells you:
 # "here's what's configured, and here's whether that's good or bad." It doesn't fix anything —
 #  it just reports.
-# We start with the SSH config check
-# We are using a while loop to read the /etc/ssh/sshd_config file to see if Root SSH login is enabled/disable
+
 set -u
 SettingName=${1:-}
 usage(){
         echo -e "\nusage:\n\t$0 <SettingName> [...]\n" >&2
 }
+
 matches_found=0
+
 if [[ -z $SettingName ]]
 then
         usage
         exit 1
 fi
+
 while read -r line
 do
         matches_found=$((matches_found + 1))
         echo ""
         echo "$line"
+
         # Check if line is commented or not
+	
         if [[ "$line" =~ ^[[:space:]]*# ]]
         then
                 echo "This line is Commented"
                 echo ""
-        elif [[ "$line" =~ ^[[:space:]]*$SettingName ]]
+		
+	elif [[ "$line" =~ ^[[:space:]]*$SettingName && "$SettingName" == "PasswordAuthentication" ]]
+	then
+                keyword=$(echo "$line" | awk '{ print $2 }')
+                if [[ "$keyword" == "no" ]]
+                then
+			echo "[PASS] disables password login and forces SSH key-based authentication. (Safer)"
+                        echo ""
+
+                elif [[ "$keyword" == "yes" ]]
+                then
+			echo "[FAIL] allows password login, which can be targeted by brute-force attacks (Riskier)"
+                        echo ""
+                else
+                        echo "[Warning] Unknown Value: $keyword"
+                        echo ""
+                fi
+
+
+        elif [[ "$line" =~ ^[[:space:]]*$SettingName && "$SettingName" == "PermitRootLogin" ]]
         then
                 keyword=$(echo "$line" | awk '{ print $2 }')
                 if [[ "$keyword" == "no" ]]
